25bfa33b4eaa159212f52d1735c3bf36d5d02866
Aldrin <aldrinnicolassalazaravilas@gmail.com>
2026-06-29 17:37:12 -0500

    Descripción de los cambios

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ def inicio():
 def partes_del_computador_1():
       return render_template("juegos/buscar_partes_de_computer.html")
 
+
+
+
+@app.route("/robot_controler")
+def robot_controller():
+   return render_template("juegos/robot_controler.html")
+   
 #seccion de Guias#
 
 
