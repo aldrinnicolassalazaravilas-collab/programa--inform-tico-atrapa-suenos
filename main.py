34f9b304a407ce5e83dcdf055af16d1428474bcc
Aldrin <aldrinnicolassalazaravilas@gmail.com>
2026-06-29 15:55:05 -0500

    Subiendo mi proyecto

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio(): 
+#main#
+ return render_template("AI del programa/asistente_ai.html")
+
+
+
+
+
+
+#seccion de juegos#
+
+@app.route("/Partes del computador")
+def partes_del_computador_1():
+      return render_template("juegos/buscar_partes_de_computer.html")
+
+#seccion de Guias#
+
+
+
+
+if __name__ == "__main__":
+   print("Iniciando servidor")
+   app.run(debug=True)
